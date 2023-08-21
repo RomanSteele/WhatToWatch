515ccc4821f4ad9f18a50400934595b4cad75077
«Roman <romanzelenenko@yandex.ru>
2023-08-21 14:24:43 +0200

    feat: add mocks

---
+++ project/src/mocks/reviews.ts
@@ -0,0 +1,15 @@
+import { Review } from "../types/review";
+
+
+export const Movies: Review[] = [
+
+  {"id":1,
+  "user":{"id":19,"name":"Christina"},
+  "rating":3.6,
+  "comment":"This movie is perfect in all its categories: credits, sound track, production, casting, writing, photography, editing, acting, and direction.\nI was amazed with the freedom of the use of the camera. This movie will change the way movies are made. Slow-mo, stills, black and white, and color were all used to brilliant effect.","date":"2023-07-20T07:05:34.295Z"},
+
+  {"id":2,"user":{"id":13,"name":"Zak"},
+  "rating":1,
+  "comment":"The editing is a mess, and the transitions of the plot or characters are rather strange. There is no narrative focus and the storytelling is unbalanced. I cannot really understand why such a bad movie received an overwhelming approval from the critics. ",
+  "date":"2023-07-13T07:05:34.295Z"}
+]
